69e40d8fb460796e6eb9aa6d17349f8f9c108dcd
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-07-19 16:12:41 +0200

    Refactoring code to test multiple endpoints

--- request.rb
+++ request.rb
@@ -5,7 +5,8 @@ require "pry"
 module NavitiaAPI
   URL = "api.navitia.io"
   VERSION = "v1"
-  KEY = ENV.fetch("NAVITIA_API_KEY", "Expecting an environment variable called 'NAVITIA_API_KEY'. Aquire one here: http://navitia.io/register/.")
+  #KEY = ENV.fetch("NAVITIA_API_KEY", "Expecting an environment variable called 'NAVITIA_API_KEY'. Aquire one here: http://navitia.io/register/.")
+  KEY = "3b036afe-0110-4202-b9ed-99718476c2e0" # personal api key is not working, so use the example key for now...
 
   def self.base_url
     "https://#{KEY}@#{URL}/#{VERSION}"
@@ -15,22 +16,37 @@ module NavitiaAPI
     "#{base_url}/coverage"
   end
 
-  # @param [Hash] coords Coordinates like {lat: 48.873462, lon: 2.353375}
-  def self.places_nearby_endpoint(coords)
-    coords_str = "#{coords[:lat]}%3B#{coords[:lon]}" # the ";" should get translated into a "%3B"
+  # Example coordinates.
+  def self.coords
+    {lat: 48.873462, lon: 2.353375}
+  end
+
+  def self.coords_str
+    "#{coords[:lat]}%3B#{coords[:lon]}" # the ";" should get translated into a "%3B"
+  end
+
+  def self.coverage_nearby_endpoint
+    return "#{base_url}/coverage/#{coords_str}"
+  end
+
+  def self.places_nearby_endpoint
     return "#{base_url}/coord/#{coords_str}/places_nearby"
   end
 end
 
-endpoint = NavitiaAPI.coverage_endpoint
-puts "ENDPOINT -- #{endpoint}"
-response = HTTParty.get(endpoint)
-puts response.body, response.code, response.message, response.headers.inspect
+#endpoint = NavitiaAPI.coverage_endpoint
+#puts "ENDPOINT -- #{endpoint}"
+#response = HTTParty.get(endpoint)
+#puts response.body, response.code, response.message, response.headers.inspect
 
-
-
-coords = {lat: 48.873462, lon: 2.353375}
-endpoint = NavitiaAPI.places_nearby_endpoint(coords)
-puts "ENDPOINT -- #{endpoint}"
-response = HTTParty.get(endpoint)
-puts JSON.parse(response.body), response.code, response.message, response.headers.inspect
+[
+  NavitiaAPI.coverage_endpoint,
+  NavitiaAPI.coverage_nearby_endpoint,
+  NavitiaAPI.places_nearby_endpoint,
+].each do |endpoint|
+  puts "\n"
+  puts "ENDPOINT -- #{endpoint}"
+  response = HTTParty.get(endpoint)
+  puts "RESULT -- #{response.code}: #{response.message}"
+  puts "MESSAGE -- #{JSON.parse(response.body)["message"]}"  #response.headers.inspect, JSON.parse(response.body)
+end
