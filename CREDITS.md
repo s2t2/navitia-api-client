5058cce15c96b468703a33d2c3f6de76696216e1
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-07-19 17:03:41 +0200

    Fixing lat/long bug; executing successful requests using various endpoint configurations

--- CREDITS.md
+++ CREDITS.md
@@ -4,4 +4,12 @@
 
   + http://www.navitia.io/
   + http://doc.navitia.io/
-  + http://doc.navitia.io/#places-nearby
+  + http://doc.navitia.io/#api-catalog
+    + http://doc.navitia.io/#coverage
+    + http://doc.navitia.io/#places-nearby
+  + http://canaltp.github.io/navitia-playground/
+  + https://groups.google.com/forum/#!forum/navitia
+
+## JavaScript Client
+
+  + http://jsfiddle.net/gh/get/jquery/2.2.2/CanalTP/navitia/tree/documentation/slate/source/examples/jsFiddle/lines/
--- request.rb
+++ request.rb
@@ -6,47 +6,74 @@ module NavitiaAPI
   URL = "api.navitia.io"
   VERSION = "v1"
   #KEY = ENV.fetch("NAVITIA_API_KEY", "Expecting an environment variable called 'NAVITIA_API_KEY'. Aquire one here: http://navitia.io/register/.")
-  KEY = "3b036afe-0110-4202-b9ed-99718476c2e0" # personal api key is not working, so use the example key for now...
+  KEY = "3b036afe-0110-4202-b9ed-99718476c2e0" # personal api key is not working for all endpoints at this time, so using the example key for now...
 
   def self.base_url
     "https://#{KEY}@#{URL}/#{VERSION}"
   end
 
-  def self.coverage_endpoint
+  #
+  # ENDPOINTS
+  #
+
+  def self.regions_endpoint
     "#{base_url}/coverage"
   end
 
-  # Example coordinates.
+  def self.region_endpoint
+    "#{base_url}/coverage/#{region_id}"
+  end
+
+  def self.coords_endpoint
+    "#{base_url}/coverage/#{coords_param}"
+  end
+
+  def self.places_nearby_endpoint1
+    "#{base_url}/coord/#{coords_param}/places_nearby"
+  end
+
+  def self.places_nearby_endpoint2
+    "#{base_url}/coverage/#{coords_param}/coords/#{coords_param}/places_nearby"
+  end
+
+  def self.places_nearby_endpoint3
+    "#{base_url}/coverage/#{region_id}/coords/#{coords_param}/places_nearby"
+  end
+
+  ##def self.places_nearby_endpoint4
+  ##  "#{base_url}/coverage/#{region_id}/#{resource_path}/places_nearby"
+  ##end
+
+  #
+  # METHODS TO BE MOVED ELSEWHERE...
+  #
+
+  def self.region_id
+    "sandbox"
+  end
+
   def self.coords
     {lat: 48.873462, lon: 2.353375}
   end
 
-  def self.coords_str
-    "#{coords[:lat]}%3B#{coords[:lon]}" # the ";" should get translated into a "%3B"
-  end
-
-  def self.coverage_nearby_endpoint
-    return "#{base_url}/coverage/#{coords_str}"
-  end
-
-  def self.places_nearby_endpoint
-    return "#{base_url}/coord/#{coords_str}/places_nearby"
+  def self.coords_param
+    "#{coords[:lon]};#{coords[:lat]}"
   end
 end
 
-#endpoint = NavitiaAPI.coverage_endpoint
-#puts "ENDPOINT -- #{endpoint}"
-#response = HTTParty.get(endpoint)
-#puts response.body, response.code, response.message, response.headers.inspect
-
 [
-  NavitiaAPI.coverage_endpoint,
-  NavitiaAPI.coverage_nearby_endpoint,
-  NavitiaAPI.places_nearby_endpoint,
+  NavitiaAPI.regions_endpoint,
+  NavitiaAPI.region_endpoint,
+  NavitiaAPI.coords_endpoint,
+  NavitiaAPI.places_nearby_endpoint1,
+  NavitiaAPI.places_nearby_endpoint2,
+  NavitiaAPI.places_nearby_endpoint3,
+  ##NavitiaAPI.places_nearby_endpoint4
 ].each do |endpoint|
   puts "\n"
   puts "ENDPOINT -- #{endpoint}"
   response = HTTParty.get(endpoint)
   puts "RESULT -- #{response.code}: #{response.message}"
-  puts "MESSAGE -- #{JSON.parse(response.body)["message"]}"  #response.headers.inspect, JSON.parse(response.body)
+  puts "MESSAGE -- #{JSON.parse(response.body)["message"]}" if JSON.parse(response.body)["message"] #response.headers.inspect, JSON.parse(response.body)
+  #pp JSON.parse(response.body)
 end
