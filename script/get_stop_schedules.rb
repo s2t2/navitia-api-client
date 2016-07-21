11fbe88767965776cd8c89093f2f05e7e3732600
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-07-21 19:14:28 +0200

    printing upcoming departure times, but I think the values are incorrect; #1

--- script/get_stop_schedules.rb
+++ script/get_stop_schedules.rb
@@ -1,7 +1,17 @@
+require "httparty"
+require "pry"
+
 require_relative "../lib/navitia_api"
 
+def arrival_time_label(datetime)
+  seconds_from_now = datetime.to_time - DateTime.now.to_time
+  minutes_from_now = (seconds_from_now / 60).floor
+  return "arriving" if minutes_from_now <= 1
+  return "#{minutes_from_now.to_s} mins"
+end
+
 coords = NavitiaAPI::Coords.new
-endpoint = NavitiaAPI.stop_schedules_endpoint(coords.parameterized)
+endpoint = NavitiaAPI::Endpoints.stop_schedules(coords.parameterized)
 puts "ENDPOINT -- #{endpoint}"
 response = HTTParty.get(endpoint)
 puts "RESULT -- #{response.code}: #{response.message}"
@@ -10,10 +20,6 @@ puts "MESSAGE -- #{parsed_body["message"]}" if parsed_body["message"]
 #puts parsed_body.keys #> ["stop_schedules", "pagination", "links", "disruptions", "notes", "feed_publishers", "exceptions"]
 puts "PAGINATION -- #{parsed_body["pagination"]}" #> {"start_page"=>0, "items_on_page"=>2, "items_per_page"=>10, "total_result"=>2}
 #TODO: handle pagination
-
-#
-# DISTINGUISH BETWEEN TRANSIT MODES
-#
 
 metro_schedules = parsed_body["stop_schedules"].select{|ss| ss["display_informations"]["commercial_mode"] == "Metro"}
 bus_schedules = parsed_body["stop_schedules"].select{|ss| ss["display_informations"]["commercial_mode"] == "Bus"}
@@ -24,26 +30,33 @@ metro_station_names = metro_schedules.map{|sched| sched["stop_point"]["name"] }.
 metro_station_names.each do |station_name|
   puts "\n"
   puts "hello, you are at station #{station_name.downcase}"
-
   puts "\n"
   puts "-----------"
   puts "next trains"
   puts "-----------"
 
+  metro_schedules = metro_schedules.select{|sched| sched["stop_point"]["name"] == station_name }
   metro_schedules.each do |stop_schedule| #> ["stop_point", "links", "date_times", "route", "additional_informations", "display_informations"]
-    raise "INVESTIGATE LINE LABEL" unless [ stop_schedule["display_informations"]["code"], stop_schedule["display_informations"]["label"], stop_schedule["display_informations"]["network"] ].uniq != 1
-
+    di = stop_schedule["display_informations"]
+    line_labels = [ di["code"], di["label"] ]
+    raise "INVESTIGATE LINE LABEL(S) -- #{line_labels.uniq}" unless line_labels.uniq.count == 1
     display = {
-      :transportation_method => stop_schedule["display_informations"]["commercial_mode"],
-      :line_label => stop_schedule["display_informations"]["label"],
-      :line_color => stop_schedule["display_informations"]["color"],
-      :line_text_color => stop_schedule["display_informations"]["text_color"],
-      :direction_name => stop_schedule["display_informations"]["direction"],
+      :transportation_method => di["commercial_mode"],
+      :line_label => di["label"],
+      :line_color => di["color"],
+      :line_text_color => di["text_color"],
+      :direction_name => di["direction"],
     }
 
     puts "\n"
     puts "direction: #{display[:direction_name].downcase.gsub("(paris)","")}"
-    puts " + line: #{display[:line_label]} (##{display[:line_text_color]} on ##{display[:line_color]})"
+    puts "  line: #{display[:line_label]} (##{display[:line_text_color]} on ##{display[:line_color]})"
+
+    departures = stop_schedule["date_times"].map{|dt| DateTime.parse(dt["date_time"]) }
+    upcoming_departures = departures.sort.first(2)
+    upcoming_departure_labels = upcoming_departures.map{|dt| arrival_time_label(dt) }
+
+    puts "    next trains arriving in: #{upcoming_departure_labels.first} and #{upcoming_departure_labels[1]}"
 
     #route = { #> ["direction", "codes", "name", "links", "is_frequence", "geojson", "direction_type", "line", "id"]
     #  :id => stop_schedule["route"]["id"],
